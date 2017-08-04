d1bdf3dd4aee2dc61fa2f93dbb34439e486f3a73
Samuel Ryzek <samuelryzek@Samuels-MacBook-Pro.local>
2017-08-04 15:21:37 -0600

    MakeChange Rev1

---
+++ src/MakeChange.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class MakeChange 
+{
+	public static void main(String[] args) 
+	{
+		Scanner kb = new Scanner(System.in);
+		getMoney();
+		makeChange100();
+		makeChange25();
+		makeChange10();
+		makeChange5();
+		makeChange1();
+	}
+
+
+	public static double getMoney()
+	{
+		return -1.0;
+	}
+	
+	public static double makeChange100()
+	{
+		return -1.0;
+	}
+	
+	public static double makeChange25()
+	{
+		return -1.0;
+	}
+	
+	public static double makeChange10()
+	{
+		return -1.0;
+	}
+	public static double makeChange5()
+	{
+		return -1.0;
+	}
+	public static double makeChange1()
+	{
+		return -1.0;
+	}
+	
+}
