f051e6453e4ef3b65e6604fc82030bcc8ae11636
Samuel Ryzek <samuelryzek@Samuels-MacBook-Pro.local>
2017-08-04 15:38:02 -0600

    Update 2

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -2,42 +2,53 @@ import java.util.*;
 
 public class MakeChange 
 {
+	Scanner kb = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
 		Scanner kb = new Scanner(System.in);
-		getMoney();
-		makeChange100();
-		makeChange25();
-		makeChange10();
-		makeChange5();
-		makeChange1();
+		double cashIn, amountDue, cashOut;
+		int pennies = 0;
+		
+		System.out.println("How much is the item: ");
+		amountDue = kb.nextDouble();
+		
+		
+		getMoney(pennies);
+		makeChange100(pennies);
+		makeChange25(pennies);
+		makeChange10(pennies);
+		makeChange5(pennies);
+		makeChange1(pennies);
 	}
 
 
-	public static double getMoney()
+	public static double getMoney(int pennies) //This method gets money from the customer.
 	{
 		return -1.0;
 	}
 	
-	public static double makeChange100()
+	public static double makeChange100(int pennies)//This method will break the amount given into dollars.
+	
 	{
 		return -1.0;
 	}
 	
-	public static double makeChange25()
+	public static double makeChange25(int pennies)//This method will break the amount down by quarters.
 	{
 		return -1.0;
 	}
 	
-	public static double makeChange10()
+	public static double makeChange10(int pennies)//This method will break the amount down by dimes.
 	{
 		return -1.0;
 	}
-	public static double makeChange5()
+	
+	public static double makeChange5(int pennies)//This method will break the amount down by nickels.
 	{
 		return -1.0;
 	}
-	public static double makeChange1()
+	
+	public static double makeChange1(int pennies)//This method will break the amount down by pennies.
 	{
 		return -1.0;
 	}
