d937d22f2f2bb5cf7fea01d6dc1fa3bd32ebb94b
Samuel Ryzek <samuelryzek@Samuels-MacBook-Pro.local>
2017-08-05 09:42:37 -0600

    Program works no methods.

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -8,23 +8,38 @@ public class MakeChange
 	{
 		kb2 = new Scanner(System.in);
 		double cashIn, amountDue, cashOut, penniesR;
-		int pennies = 0, penniesT, quarters, dimes, nickels;
+		int pennies = 0, penniesT, quarters, dimes, nickels, dollars, fiveDollars, tenDollars, twentyDollars;
 		
 		System.out.print("How much is the item: $");
-		amountDue = kb2.nextFloat();
+		amountDue = kb2.nextDouble();
 		System.out.print("How much money was tendered? $");
-		cashIn = kb2.nextFloat();
+		cashIn = kb2.nextDouble();
+		
+		if(cashIn < amountDue)
+		{
+			System.err.println("Customer has not provided enough funds!");
+		}
+		
 		
 		cashOut = cashIn - amountDue;
-		
-		System.out.println("$" + cashOut);
-		
 		penniesR = Math.round(cashOut*100.00)/100.00;
-		System.out.println(penniesR);
+		//System.out.println(penniesR);						//check to ensure that math is correct
 		
+		System.out.println("Customer's change $" + penniesR);
+		
+		
+	
 		pennies = (int)Math.round(cashOut * 100);
-		System.out.println(pennies);
+		//System.out.println(pennies);						//check to ensure that math is correct
 		
+		twentyDollars = pennies/2000;
+		pennies %= 2000;
+		tenDollars = pennies/1000;
+		pennies %= 1000;
+		fiveDollars = pennies/500;
+		pennies %= 500;
+		dollars = pennies/100;
+		pennies %= 100;		
 		quarters = pennies/25;
 		pennies %= 25;
 		dimes = pennies/10;
@@ -34,8 +49,15 @@ public class MakeChange
 		penniesT = pennies/1;
 		pennies %= 1;
 		
-		System.out.println("Quarters : " + quarters + " Dimes : " + dimes +
-				" Nickels : " + nickels + " Pennies : " + penniesT);
+		System.out.println("Customer's change by denomination: ");
+		System.out.println("Twenties: " + twentyDollars);
+		System.out.println("Tens: " + tenDollars);
+		System.out.println("Fives: " + fiveDollars);
+		System.out.println("Ones: " + dollars);
+		System.out.println("Quarters : " + quarters);
+		System.out.println("Dimes : " + dimes);
+		System.out.println("Nickels : " + nickels);
+		System.out.println("Pennies : " + penniesT);
 	}
 	
 }
